6f4695a1be39c7221a08181db2275a2ad2e8b4e4
Tina <longevity.tina@gmail.com>
2020-01-04 13:19:44 -0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,26 @@
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
+let cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+cardsInPlay = [];
+
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+console.log("user flipped " + cardOne);
+console.log("user flipped " + cardTwo);
+
+
+if (cardsInPlay.length === 2){
+} 
+  if (cardsInPlay[0] === cardsInPlay[1]){
+    alert("You found a match!");
+} else {
+    alert("sorry, try agian");
+}
+
+
+
+
+

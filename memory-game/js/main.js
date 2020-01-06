e47d921887e9a3cd4600984ceda7eef77572df18
Tina <longevity.tina@gmail.com>
2020-01-05 17:00:41 -0800

    added functions to clean up game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -2,25 +2,26 @@ let cards = ["queen", "queen", "king", "king"];
 
 cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log("user flipped " + cardOne);
-console.log("user flipped " + cardTwo);
-
-
-if (cardsInPlay.length === 2){
-} 
-  if (cardsInPlay[0] === cardsInPlay[1]){
-    alert("You found a match!");
-} else {
-    alert("sorry, try agian");
+function checkForMatch(){
+    if(cardsInPlay[0] === cardsInPlay[1]){
+        console.log("You found a match!");
+    } else {
+        console.log("Sorry, try again.");
+    }
 }
 
+function flipCard(cardId){
 
+    cardsInPlay.push(cards[cardId]);
+
+    console.log("User flipped " + cards[cardId]);
+    if (cardsInPlay.length === 2){ 
+    checkForMatch();
+    }
+};
+
+flipCard(0);
+flipCard(1);
 
 
 

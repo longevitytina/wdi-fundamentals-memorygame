bef260cbe05b2222236f1320082a57976d259130
Tina <longevity.tina@gmail.com>
2020-01-06 13:55:46 -0800

    updated game logic with objects

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,20 +1,46 @@
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {        
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    }
+];
+        
 
 cardsInPlay = [];
 
 function checkForMatch(){
     if(cardsInPlay[0] === cardsInPlay[1]){
         console.log("You found a match!");
+        alert("Winner!");
     } else {
         console.log("Sorry, try again.");
+        alert("Sorry, try again");
     }
 }
 
 function flipCard(cardId){
 
-    cardsInPlay.push(cards[cardId]);
+    cardsInPlay.push(cards[cardId].rank);
 
-    console.log("User flipped " + cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
     if (cardsInPlay.length === 2){ 
     checkForMatch();
     }
@@ -22,6 +48,5 @@ function flipCard(cardId){
 
 flipCard(0);
 flipCard(1);
-
 
 

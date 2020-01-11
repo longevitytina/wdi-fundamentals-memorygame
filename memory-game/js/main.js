c0f45841f23beb60f5519eb9181ec4e8c065182b
Tina <longevity.tina@gmail.com>
2020-01-11 10:42:20 -0800

    added flp card game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -17,28 +17,30 @@ let cards = [
     {
         rank: "king",
         suit: "diamonds",
-        cardImage: "images/queen-of-diamonds.png"
+        cardImage: "images/king-of-diamonds.png"
     }
 ];
         
-
 cardsInPlay = [];
 
 function checkForMatch(){
-    if(cardsInPlay[0] === cardsInPlay[1]){
+    
+
+    if(cardsInPlay.rank === cardsInPlay.rank){
         console.log("You found a match!");
         alert("Winner!");
     } else {
         console.log("Sorry, try again.");
         alert("Sorry, try again");
     }
-}
+};
 
-function flipCard(cardId){
-
+function flipCard(){
+    var cardId = this.getAttribute('data-id');
+    this.setAttribute('src', cards[cardId].cardImage);
     cardsInPlay.push(cards[cardId].rank);
 
-    console.log("User flipped " + cards[cardId].rank);
+    console.log('User flipped ' + cards[cardId].rank);
     console.log(cards[cardId].cardImage);
     console.log(cards[cardId].suit);
     if (cardsInPlay.length === 2){ 
@@ -46,7 +48,16 @@ function flipCard(cardId){
     }
 };
 
-flipCard(0);
-flipCard(1);
+function createBoard(){
+    for(let i = 0; i < cards.length; i++){
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        document.getElementById('game-board').appendChild(cardElement);
+        cardElement.addEventListener('click', flipCard)
+    }
+}
 
 
+
+createBoard();

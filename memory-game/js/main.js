67ca7b3d0e106437f630f7348dcd023315fc6152
Tina <longevity.tina@gmail.com>
2020-02-08 12:06:00 -0800

    Give ability for computer to play until no matches

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -4,7 +4,7 @@ let cards = [
         suit: "hearts",
         cardImage: "images/queen-of-hearts.png"
     },
-    {        
+    {
         rank: "queen",
         suit: "diamonds",
         cardImage: "images/queen-of-diamonds.png"
@@ -63,31 +63,35 @@ var playerTwo = {
     scoreDisplay: document.querySelector('#player-2-score'),
 };
 var activePlayer = playerOne;
-var cardBack = 'images/back.png'; 
+var computerPlayer;
+var cardBack = 'images/back.png';
 shuffle(cards);
 
 function shuffle(array) {
     var m = array.length, t, i;
-  
+
     // While there remain elements to shuffle…
     while (m) {
-      // Pick a remaining element…
-      i = Math.floor(Math.random() * m--);
-      // And swap it with the current element.
-      t = array[m];
-      array[m] = array[i];
-      array[i] = t;
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+        // And swap it with the current element.
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
     }
     return array;
-  };
+};
 
-function checkForMatch(){
-    var lastSelected = cardsInPlay[cardsInPlay.length-1];
-    var penultimate = cardsInPlay[cardsInPlay.length-2];
+function checkForMatch() {
+    var lastSelected = cardsInPlay[cardsInPlay.length - 1];
+    var penultimate = cardsInPlay[cardsInPlay.length - 2];
 
 
-    if(cards[lastSelected].rank === cards[penultimate].rank){
+    if (cards[lastSelected].rank === cards[penultimate].rank) {
         incrementScore();
+        if (activePlayer === computerPlayer) {
+            computerTurn();
+        }
         console.log("You found a match!");
     } else {
         var lastCard = document.querySelector(`[data-id="${lastSelected}"]`)
@@ -101,20 +105,20 @@ function checkForMatch(){
     }
 };
 
-function incrementScore(){
+function incrementScore() {
     activePlayer.score++
     activePlayer.scoreDisplay.innerHTML = activePlayer.score
 };
 
-function flipCard(){
+function flipCard() {
     var cardId = this.getAttribute('data-id');
-    if (this.getAttribute('src') === cardBack){
+    if (this.getAttribute('src') === cardBack) {
         this.setAttribute('src', cards[cardId].cardImage);
         cardsInPlay.push(cardId);
         console.log('User flipped ' + cards[cardId].rank);
         console.log(cards[cardId].cardImage);
         console.log(cards[cardId].suit);
-        if (cardsInPlay.length % 2 === 0 ){ 
+        if (cardsInPlay.length % 2 === 0) {
             setTimeout(checkForMatch, 500);
         }
     }
@@ -122,13 +126,17 @@ function flipCard(){
 
 document.querySelector('.btn-new').addEventListener('click', init);
 
-function init(){
+function init() {
     const gameBoard = document.getElementById('game-board');
     activePlayer = playerOne;
-    while (gameBoard.firstChild){
+    while (gameBoard.firstChild) {
         gameBoard.removeChild(gameBoard.firstChild);
     }
-    score = 0;
+    computerPlayer = null;
+    playerOne.score = 0;
+    playerTwo.score = 0;
+    playerOne.scoreDisplay.innerHTML = playerOne.score;
+    playerTwo.scoreDisplay.innerHTML = playerTwo.score;
     cardsInPlay.length = 0;
     createBoard();
     document.querySelector('#player-1-label').classList.remove('active');
@@ -137,10 +145,10 @@ function init(){
 
 };
 
-function createBoard(){
+function createBoard() {
     shuffle(cards);
-    
-    for(let i = 0; i < cards.length; i++){
+
+    for (let i = 0; i < cards.length; i++) {
         var cardElement = document.createElement('img');
         cardElement.setAttribute('src', cardBack);
         cardElement.setAttribute('data-id', i);
@@ -149,11 +157,14 @@ function createBoard(){
     }
 };
 
-function nextPlayer(){
-    if (activePlayer === playerOne){
+function nextPlayer() {
+    if (activePlayer === playerOne) {
         activePlayer = playerTwo
     } else {
         activePlayer = playerOne
+        if (playerOne === computerPlayer) {
+            computerTurn()
+        }
     }
 
     document.querySelector('#player-1-label').classList.toggle('active')
@@ -161,22 +172,25 @@ function nextPlayer(){
 };
 
 createBoard();
+document.querySelector('.btn-computer').addEventListener('click', function () {
+    init();
+    computerTurn();
+    computerPlayer = playerOne;
+});
+// var computer = playerOne;
+//cards that have been seen but not matched
+// var cardsFaceDown = [];
+//if no cards flipped, then flip two cards at random if turn is active
+// if (activePlayer === computer && cardsInPlay.length === 0){
+//click two cards and end turn if no match
+// document.getElementById('data-id').click();
+// }
 
-document.querySelector('.btn-computer').addEventListener('click', computerTurn);
-    // var computer = playerOne;
-    //cards that have been seen but not matched
-    // var cardsFaceDown = [];
-    //if no cards flipped, then flip two cards at random if turn is active
-    // if (activePlayer === computer && cardsInPlay.length === 0){
-        //click two cards and end turn if no match
-        // document.getElementById('data-id').click();
-        // }
 
-
-function computerTurn(){
-    for(var i = 0; i<2;i++ ){
-        var randomIndex = Math.floor(Math.random()* cards.length);
-        if (cardsInPlay.includes(randomIndex.toString())){
+function computerTurn() {
+    for (var i = 0; i < 2; i++) {
+        var randomIndex = Math.floor(Math.random() * cards.length);
+        if (cardsInPlay.includes(randomIndex.toString())) {
             i--
             continue
         }
@@ -190,17 +204,17 @@ function computerTurn(){
 //attempts score
 
 /**
- * computer vs human 
- * btn: pick multiplayer 
+ * computer vs human
+ * btn: pick multiplayer
  * or
  * btn: computer
  * computer turn
  * first turn:empty cardArray-two random cards
- * second turn: depending on third card flipped, decide to flip new card or 
+ * second turn: depending on third card flipped, decide to flip new card or
  * previous card, if
- * 
+ *
  * how to separate and organize different modes
  * computer mode - easy, med, hard mode?
- * multiplayer -current code 
- * 
+ * multiplayer -current code
+ *
  */
